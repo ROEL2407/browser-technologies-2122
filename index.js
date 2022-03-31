a6c4d9fb7697bdbd4b40e30401e885cb70171750
ROEL2407 <roel24komen@gmail.com>
2022-03-31 11:48:04 +0200

    Create: views map added

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 5000;                  //Save the port number where your server will be listening
 
+app.set("view engine", "ejs");
+app.set("views", "./views");
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+    res.render("home");      //server responds by sending the index.html file to the client's browser
                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
 
